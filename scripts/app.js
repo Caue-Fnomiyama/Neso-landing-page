cfaad4b9f35115da4354d8b339081d44f14f1ff1
Caue-Fnomiyama <cauenomiyama@gmail.com>
2026-04-23 00:56:03 -0300

    Add files via upload

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,8 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
+const logo = document.getElementById("logoPrincipal");
 
-hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  logo.classList.toggle("hide");
+});
